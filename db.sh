862d85807cb9f8c0ad6f3e37978fed4980110b09
antleypk <peterantley@gmail.com>
2019-10-07 17:08:51 +0000

    create db & table for holding movie data

---
+++ db.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -exou pipefail
+echo "build table to hold data"
+sudo mysql -h localhost -u root --password='rh3lt35t' rh_test < movie_metadata.sql || echo "failed to create table"
+echo "load movie data into table 'rh_test.movie_metadata'"
