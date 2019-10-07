1302045349bfcc92fabf36723c95a7820d6bf3bd
antleypk <peterantley@gmail.com>
2019-10-07 18:52:07 +0000

    db.sh loads all records, forgot to git add first time

--- db.sh
+++ db.sh
@@ -3,3 +3,4 @@ set -exou pipefail
 echo "build table to hold data"
 sudo mysql -h localhost -u root --password='rh3lt35t' rh_test < movie_metadata.sql || echo "failed to create table"
 echo "load movie data into table 'rh_test.movie_metadata'"
+sudo python3 data_loader.py
