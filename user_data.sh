a035fc0889b091545f84cb6c54be76341c61e7e9
antleypk <peterantley@gmail.com>
2019-10-08 03:26:48 +0000

    updated init

--- user_data.sh
+++ user_data.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
+#running this bash script on a new rhel 8 image will produce desired results
+
+#install git
 sudo yum install git -y
+
+#grab the application code
 git clone https://github.com/antleypk/red_hat_test.git
+
+#move to application folder
 cd red_hat_test
+
+#initialize the application
 ./init.sh &> out
